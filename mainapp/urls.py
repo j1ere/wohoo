5b112ba340fc85e924926a23140c25bf3f0223fe
j1ere <jeremywambua106@gmail.com>
2024-10-22 13:00:30 +0300

    index.html update

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("" ,views.chat_view)
+    path("<str:room_name>" ,views.chat_view)
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def chat_view(request):
-    return render(request, 'mainapp/index.html')
+def chat_view(request, room_name):
+    return render(request, 'mainapp/index.html', {'room_name': room_name})
